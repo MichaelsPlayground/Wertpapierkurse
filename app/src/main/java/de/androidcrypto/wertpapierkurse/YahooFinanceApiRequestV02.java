6fa957f762addb2d5ad469d5b75fbd1edc8dc4c3
michaelfehr <michael_fehrgmx.de>
2022-03-12 16:25:40 +0100

    updated

---
+++ app/src/main/java/de/androidcrypto/wertpapierkurse/YahooFinanceApiRequestV02.java
@@ -0,0 +1,312 @@
+package de.androidcrypto.wertpapierkurse;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class YahooFinanceApiRequestV02 {
+
+    //
+    public static void main(String[] args) throws IOException {
+        // https://www.yahoofinanceapi.com/pricing
+
+
+        // get symbols: https://de.finance.yahoo.com
+        // yahoo docs: https://www.yahoofinanceapi.com/tutorial
+        // api docs: https://www.yahoofinanceapi.com
+        // mk9W3hgZK056nOQBrwuH5tMBGZAHOwD6EbFVNwt7
+        // ishares commodities: EXXY.DE
+        // xtrackers s&p500 equal weight XDEW.DE  Xtrackers S&P 500 Equal Weight UCITS ETF (XDEW.L)
+
+
+        // get name
+        System.out.println("*** get name ***");
+
+        /*
+        curl -X 'GET' \
+  'https://yfapi.net/v8/finance/spark?interval=1d&range=6mo&symbols=%20XDEW.DE' \
+  -H 'accept: application/json' \
+  -H 'X-API-KEY: mk9W3hgZK056nOQBrwuH5tMBGZAHOwD6EbFVNwt7'
+         */
+        // https://yfapi.net/v8/finance/spark?interval=1d&range=6mo&symbols=%20XDEW.DE
+
+        //URL urlName = new URL("https://data.lemon.markets/v1/instruments/?isin=IE00BJ0KDQ92&mic=XMUN");
+        URL urlName = new URL("https://yfapi.net/v8/finance/spark?interval=1d&range=6mo&symbols=%20XDEW.DE");
+        HttpURLConnection httpName = (HttpURLConnection) urlName.openConnection();
+
+        httpName.setRequestProperty("X-API-KEY", "mk9W3hgZK056nOQBrwuH5tMBGZAHOwD6EbFVNwt7");
+
+        System.out.println(httpName.getResponseCode() + " ResponseMessage: " + httpName.getResponseMessage());
+
+        BufferedReader brName = new BufferedReader(new InputStreamReader((httpName.getInputStream())));
+        StringBuilder sbName = new StringBuilder();
+        String outputName;
+        while ((outputName = brName.readLine()) != null) {
+            sbName.append(outputName);
+        }
+        String dataName = sbName.toString();
+        System.out.println("Content:\n" + dataName);
+
+        // split string
+        System.out.println("split string");
+        String[] parts = dataName.split(",");
+        System.out.println("parts: " + parts.length);
+        for (int i = 0; i < parts.length; i++) {
+            System.out.println("p: " + i + " d: " + parts[i]);
+        }
+        System.out.println("*** END ***");
+
+        System.out.println("responseMethod: " + httpName.getRequestMethod());
+
+        System.out.println("search substring timestamp\":[");
+        int pos = searchPosition(parts[0], "timestamp\":[");
+        System.out.println("pos: " + pos);
+        String firstData = parts[0].substring(pos, parts[0].length());
+        System.out.println("firstData: " + firstData);
+
+        int endTimestamp = 0;
+        for (int i = 1; i < parts.length; i++) {
+            int posEnd = searchPosition(parts[i], "]");
+            if (posEnd > -1) {
+                System.out.println("end timestamp found at " + i + " posEnd: " + posEnd);
+                endTimestamp = i;
+                break; // end for
+            }
+        }
+        System.out.println("endTimestamp: " + endTimestamp);
+        parts[endTimestamp] = parts[endTimestamp].replace("]", "");
+        System.out.printf("last timestamp: " + parts[endTimestamp]);
+
+        System.out.println("find priceClose");
+        for (int i = endTimestamp; i < parts.length; i++) {
+            int posEnd = searchPosition(parts[i], "close\":[");
+            if (posEnd > -1) {
+                System.out.println("first closePrice found at " + i + " posEnd: " + posEnd);
+                break; // end for
+            }
+        }
+
+
+        // get data
+        // parseName(dataName); // yahoo responds no name
+
+        // read only year end value
+        System.out.println("*** get prices ***");
+        /*
+        URL url = new URL("https://data.lemon.markets/v1/ohlc/d1/?isin=IE00BJ0KDQ92&mic=XMUN&from=2021-12-30&to=2022-01-03");
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+
+        http.setRequestProperty("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJsZW1vbi5tYXJrZXRzIiwiaXNzIjoibGVtb24ubWFya2V0cyIsInN1YiI6InVzcl9xeURNZFdXUlJDd1JQOGhHME1HZkxscDZTZkZNa3lYenNUIiwiZXhwIjoxNjc1OTg0Njg0LCJpYXQiOjE2NDQ0NDg2ODQsImp0aSI6ImFwa19xeURNZFhYR0dENFFjU0psVm04S1k1Ump4Y25GbnBHcjRrIiwibW9kZSI6InBhcGVyIn0.Li0sacTPoJHdFiSp-yNQ_lPUeDFgR15V1_VHPZGZel0");
+
+        System.out.println(http.getResponseCode() + " ResponseMessage: " + http.getResponseMessage());
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((http.getInputStream())));
+        StringBuilder sb = new StringBuilder();
+        String output;
+        while ((output = br.readLine()) != null) {
+            sb.append(output);
+        }
+        String data = sb.toString();
+        System.out.println("Content:\n" + data);
+*/
+        //parsePrices(dataName);
+
+        // boerse frankfurt https://www.boerse-frankfurt.de/etf/xtrackers-msci-world-ucits-etf-1c/kurshistorie/historische-kurse-und-umsaetze
+        // 30.12.21	86,67	86,706	86,96	86,666	3.565.841	41.111
+        // https://www.boerse-muenchen.de/Etf/IE00BJ0KDQ92
+        /*
+        // now page 2
+        System.out.println("*** page 2 ***");
+        url = new URL("https://data.lemon.markets/v1/ohlc/d1/?isin=IE00BJ0KDQ92&mic=XMUN&from=2021-12-08&to=2022-01-05&page=2");
+        http = (HttpURLConnection) url.openConnection();
+        http.setRequestProperty("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJsZW1vbi5tYXJrZXRzIiwiaXNzIjoibGVtb24ubWFya2V0cyIsInN1YiI6InVzcl9xeURNZFdXUlJDd1JQOGhHME1HZkxscDZTZkZNa3lYenNUIiwiZXhwIjoxNjc1OTg0Njg0LCJpYXQiOjE2NDQ0NDg2ODQsImp0aSI6ImFwa19xeURNZFhYR0dENFFjU0psVm04S1k1Ump4Y25GbnBHcjRrIiwibW9kZSI6InBhcGVyIn0.Li0sacTPoJHdFiSp-yNQ_lPUeDFgR15V1_VHPZGZel0");
+
+        System.out.println(http.getResponseCode() + " ResponseMessage: " + http.getResponseMessage());
+        br = new BufferedReader(new InputStreamReader((http.getInputStream())));
+        sb = new StringBuilder();
+        output = "";
+        while ((output = br.readLine()) != null) {
+            sb.append(output);
+        }
+        data = sb.toString();
+        System.out.println("Content:\n" + data);
+
+         */
+
+        httpName.disconnect();
+
+    }
+
+    // return the first position AFTER the searchString
+    private static int searchPosition(String completeString, String searchString) {
+        int position = -1; // default, not found
+        position = completeString.indexOf(searchString);
+        if (position > -1) {
+            int length = searchString.length();
+            position = position + length;
+        }
+        return position;
+    }
+
+
+    // uses GSON
+    // import com.google.gson.JsonArray;
+    //import com.google.gson.JsonObject;
+    //import com.google.gson.JsonParser;
+    // https://devqa.io/how-to-parse-json-in-java/
+    public static void parseName(String json) {
+        //JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+        // https://stackoverflow.com/questions/60771386/jsonparser-is-deprecated
+        JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+
+        //String pageName = jsonObject.getAsJsonObject("pageInfo").get("pageName").getAsString();
+        //System.out.println(pageName);
+
+        JsonArray arr = jsonObject.getAsJsonArray("results");
+        for (int i = 0; i < arr.size(); i++) {
+            String post_id = arr.get(i).getAsJsonObject().get("name").getAsString();
+            System.out.println("Name: " + post_id);
+        }
+
+    }
+
+    public static void parseNameOrg(String json) {
+        //JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+        // https://stackoverflow.com/questions/60771386/jsonparser-is-deprecated
+        JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+
+        //String pageName = jsonObject.getAsJsonObject("pageInfo").get("pageName").getAsString();
+        //System.out.println(pageName);
+
+        JsonArray arr = jsonObject.getAsJsonArray("results");
+        for (int i = 0; i < arr.size(); i++) {
+            String post_id = arr.get(i).getAsJsonObject().get("name").getAsString();
+            System.out.println("Name: " + post_id);
+        }
+
+    }
+    public static void parsePrices(String json) {
+        //JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+        JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+
+        //String pageName = jsonObject.getAsJsonObject("pageInfo").get("pageName").getAsString();
+        //System.out.println(pageName);
+
+        JsonArray arr = jsonObject.getAsJsonArray("results");
+        for (int i = 0; i < arr.size(); i++) {
+            String isin = arr.get(i).getAsJsonObject().get("isin").getAsString();
+            System.out.println("ISIN: " + isin);
+            String last = arr.get(i).getAsJsonObject().get("l").getAsString();
+            System.out.println("Last price: " + last);
+            String time = arr.get(i).getAsJsonObject().get("t").getAsString();
+            System.out.println("Time: " + time);
+        }
+
+    }
+
+    public static void parsePricesOrg(String json) {
+        //JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+        JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+
+        //String pageName = jsonObject.getAsJsonObject("pageInfo").get("pageName").getAsString();
+        //System.out.println(pageName);
+
+        JsonArray arr = jsonObject.getAsJsonArray("timestamp");
+        for (int i = 0; i < arr.size(); i++) {
+            String isin = arr.get(i).getAsJsonObject().get("isin").getAsString();
+            System.out.println("ISIN: " + isin);
+            String last = arr.get(i).getAsJsonObject().get("l").getAsString();
+            System.out.println("Last price: " + last);
+            String time = arr.get(i).getAsJsonObject().get("t").getAsString();
+            System.out.println("Time: " + time);
+        }
+
+    }
+
+    // vahoo finance result:
+// {"XDEW.DE":{"dataGranularity":300,"timestamp":[1631516400,1631602800,1631689200,1631775600,1631862000,1632121200,1632207600,1632294000,1632380400,1632466800,1632726000,1632812400,1632898800,1632985200,1633071600,1633330800,1633417200,1633503600,1633590000,1633676400,1633935600,1634022000,1634108400,1634194800,1634281200,1634540400,1634626800,1634713200,1634799600,1634886000,1635145200,1635231600,1635318000,1635404400,1635490800,1635753600,1635840000,1635926400,1636012800,1636099200,1636358400,1636444800,1636531200,1636617600,1636704000,1636963200,1637049600,1637136000,1637222400,1637308800,1637568000,1637654400,1637740800,1637827200,1637913600,1638172800,1638259200,1638345600,1638432000,1638518400,1638777600,1638864000,1638950400,1639036800,1639123200,1639382400,1639468800,1639555200,1639641600,1639728000,1639987200,1640073600,1640160000,1640246400,1640592000,1640678400,1640764800,1640847600,1641196800,1641283200,1641369600,1641456000,1641542400,1641801600,1641888000,1641974400,1642060800,1642147200,1642406400,1642492800,1642579200,1642665600,1642752000,1643011200,1643097600,1643184000,1643270400,1643356800,1643616000,1643702400,1643788800,1643875200,1643961600,1644220800,1644307200,1644393600,1644480000,1644566400,1644825600,1644912000,1644998400,1645084800,1645171200,1645430400,1645516800,1645603200,1645689600,1645776000,1646035200,1646121600,1646208000,1646294400,1646380800,1647016559],"symbol":"XDEW.DE","previousClose":null,"chartPreviousClose":67.96,"close":[67.93,67.61,67.74,68.05,67.85,66.68,66.89,67.46,68.39,68.46,68.99,68.08,68.61,68.17,67.79,67.52,68.6,67.76,69.57,69.15,69.37,68.98,68.44,69.56,70.17,70.03,70.11,70.63,70.44,70.92,71.48,71.63,70.9,70.39,71.29,71.43,71.75,71.83,72.28,72.78,72.68,72.56,73.14,73.15,73.56,73.92,74.64,74.26,73.8,73.73,74.56,73.94,74.63,74.74,71.87,72.75,71.6,72.04,71.03,71.37,72.55,74.11,73.34,73.61,73.22,73.07,72.92,72.57,74.01,73.39,71.08,72.86,73.27,74.1,74.4,75.07,74.99,75.24,74.96,76.01,75.9,74.92,74.65,73.54,74.04,74.26,74.49,73.38,74.2,73.34,72.96,73.42,71.67,69.24,69.98,71.42,71.93,70.52,71.74,72.38,72.6,71.83,70.77,71.25,71.82,72.85,72.63,72.16,71.3,71.86,71.65,71.32,70.83,70.33,70.49,69.91,69.15,71.4,71.94,71.33,72.54,72.76,72.67,72.13],"end":null,"start":null}}
+
+/*
+    // uses fasterxml jackson
+    // import com.fasterxml.jackson.core.JsonFactory;
+    //import com.fasterxml.jackson.core.JsonProcessingException;
+    //import com.fasterxml.jackson.databind.JsonNode;
+    //import com.fasterxml.jackson.databind.ObjectMapper;
+    public static void parse(String json) throws JsonProcessingException {
+        JsonFactory factory = new JsonFactory();
+
+        ObjectMapper mapper = new ObjectMapper(factory);
+        JsonNode rootNode = mapper.readTree(json);
+
+        Iterator<Map.Entry<String,JsonNode>> fieldsIterator = rootNode.fields();
+        while (fieldsIterator.hasNext()) {
+
+            Map.Entry<String,JsonNode> field = fieldsIterator.next();
+            System.out.println("Key: " + field.getKey() + "\tValue:" + field.getValue());
+        }
+    }
+*/
+
+/*
+Historic data
+https://data.lemon.markets/v1/ohlc/d1/
+
+https://data.lemon.markets/v1/ohlc/d1/
+request = requests.get("https://data.lemon.markets/v1/ohlc/h1/?isin=isin&mic=mic&from=2021-11-01", headers={"Authorization": "Bearer YOUR-API-KEY"})
+
+eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJsZW1vbi5tYXJrZXRzIiwiaXNzIjoibGVtb24ubWFya2V0cyIsInN1YiI6InVzcl9xeURNZFdXUlJDd1JQOGhHME1HZkxscDZTZkZNa3lYenNUIiwiZXhwIjoxNjc1OTg0Njg0LCJpYXQiOjE2NDQ0NDg2ODQsImp0aSI6ImFwa19xeURNZFhYR0dENFFjU0psVm04S1k1Ump4Y25GbnBHcjRrIiwibW9kZSI6InBhcGVyIn0.Li0sacTPoJHdFiSp-yNQ_lPUeDFgR15V1_VHPZGZel0
+
+request = requests.get("https://data.lemon.markets/v1/quotes/?isin=isin&mic=mic&from=2021-11-01", headers={"Authorization": "Bearer YOUR-API-KEY"})
+
+curl https://reqbin.com/echo/get/json
+   -H "Accept: application/json"
+   -H "Authorization: Bearer {token}“
+
+curl https://data.lemon.markets/v1/quotes/?isin=isin&mic=mic&from=2021-11-01
+   -H "Authorization: Bearer
+„
+
+Gibt einen wert:
+curl https://data.lemon.markets/v1/ohlc/h1/?isin=IE00BJ0KDQ92&mic=XMUN&from=2021-12-01
+   -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJsZW1vbi5tYXJrZXRzIiwiaXNzIjoibGVtb24ubWFya2V0cyIsInN1YiI6InVzcl9xeURNZFdXUlJDd1JQOGhHME1HZkxscDZTZkZNa3lYenNUIiwiZXhwIjoxNjc1OTg0Njg0LCJpYXQiOjE2NDQ0NDg2ODQsImp0aSI6ImFwa19xeURNZFhYR0dENFFjU0psVm04S1k1Ump4Y25GbnBHcjRrIiwibW9kZSI6InBhcGVyIn0.Li0sacTPoJHdFiSp-yNQ_lPUeDFgR15V1_VHPZGZel0"
+
+Mehrere Tage:
+curl https://data.lemon.markets/v1/ohlc/d1/?isin=IE00BJ0KDQ92&mic=XMUN&from=2021-12-08&to=2022-01-05
+   -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJsZW1vbi5tYXJrZXRzIiwiaXNzIjoibGVtb24ubWFya2V0cyIsInN1YiI6InVzcl9xeURNZFdXUlJDd1JQOGhHME1HZkxscDZTZkZNa3lYenNUIiwiZXhwIjoxNjc1OTg0Njg0LCJpYXQiOjE2NDQ0NDg2ODQsImp0aSI6ImFwa19xeURNZFhYR0dENFFjU0psVm04S1k1Ump4Y25GbnBHcjRrIiwibW9kZSI6InBhcGVyIn0.Li0sacTPoJHdFiSp-yNQ_lPUeDFgR15V1_VHPZGZel0"
+
+Online curl
+https://reqbin.com/req/c-hlt4gkzd/curl-bearer-token-authorization-header-example
+
+Dort kann auch der curl code in Java konvertiert werden
+
+URL url = new URL("https://data.lemon.markets/v1/ohlc/d1/?isin=IE00BJ0KDQ92&mic=XMUN&from=2021-12-08&to=2022-01-05");
+HttpURLConnection http = (HttpURLConnection)url.openConnection();
+http.setRequestProperty("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJsZW1vbi5tYXJrZXRzIiwiaXNzIjoibGVtb24ubWFya2V0cyIsInN1YiI6InVzcl9xeURNZFdXUlJDd1JQOGhHME1HZkxscDZTZkZNa3lYenNUIiwiZXhwIjoxNjc1OTg0Njg0LCJpYXQiOjE2NDQ0NDg2ODQsImp0aSI6ImFwa19xeURNZFhYR0dENFFjU0psVm04S1k1Ump4Y25GbnBHcjRrIiwibW9kZSI6InBhcGVyIn0.Li0sacTPoJHdFiSp-yNQ_lPUeDFgR15V1_VHPZGZel0");
+
+System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+http.disconnect();
+*/
+}
+/*
+*** get name ***
+200 ResponseMessage: OK
+Content:
+{"results":[{"isin":"IE00BJ0KDQ92","wkn":"A1XB5U","name":"X(IE)-MSCI WORLD 1C","title":"XTR.(IE) - MSCI WORLD","symbol":"XDWD","type":"etf","venues":[{"name":"Börse München - Gettex","title":"Gettex","mic":"XMUN","is_open":true,"tradable":true,"currency":"EUR"}]}],"previous":null,"next":null,"total":1,"page":1,"pages":1}
+Name: X(IE)-MSCI WORLD 1C
+*** get prices ***
+200 ResponseMessage: OK
+Content:
+{"results":[{"isin":"IE00BJ0KDQ92","o":86.57,"h":86.948,"l":86.57,"c":86.7,"t":"2021-12-30T00:00:00.000+00:00","mic":"XMUN"},{"isin":"IE00BJ0KDQ92","o":86.534,"h":87.06,"l":86.248,"c":87.05,"t":"2022-01-03T00:00:00.000+00:00","mic":"XMUN"}],"previous":null,"next":null,"total":2,"page":1,"pages":1}
+ISIN: IE00BJ0KDQ92
+Last price: 86.57
+Time: 2021-12-30T00:00:00.000+00:00
+ISIN: IE00BJ0KDQ92
+Last price: 86.248
+Time: 2022-01-03T00:00:00.000+00:00
+ */
